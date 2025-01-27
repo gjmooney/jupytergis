fcb1dcd91cea7fd124a2e8f26c06cf9871f6dfb5
Greg <gjmooney@asu.edu>
2025-01-27 17:19:10 +0100

    Start adding time slider thing

--- packages/base/src/mainview/mainView.tsx
+++ packages/base/src/mainview/mainView.tsx
@@ -80,6 +80,7 @@ import StatusBar from '../statusbar/StatusBar';
 import { isLightTheme, loadFile, throttle } from '../tools';
 import CollaboratorPointers, { ClientPointer } from './CollaboratorPointers';
 import { FollowIndicator } from './FollowIndicator';
+import TemporalSlider from './TemporalSlider';
 import { MainViewModel } from './mainviewmodel';
 import { Spinner } from './spinner';
 
@@ -1723,7 +1724,7 @@ export class MainView extends React.Component<IProps, IStates> {
           );
         })}
 
-        {this._model.isTemporal && <div>temporal slider wooo</div>}
+        {this._model.isTemporal && <TemporalSlider model={this._model} />}
         <div
           className="jGIS-Mainview"
           style={{
