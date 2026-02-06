b6601a0e99bbd698b0b07cae9ffde5fe8464c09b
Greg Mooney <gregory.mooney@quantstack.net>
2026-02-06 13:25:17 +0100

    wip

--- packages/base/src/formbuilder/objectform/layer/storySegmentLayerForm.ts
+++ packages/base/src/formbuilder/objectform/layer/storySegmentLayerForm.ts
@@ -6,6 +6,51 @@ import { LayerPropertiesForm } from './layerform';
 import { ArrayFieldTemplate } from '../components/SegmentFormSymbology';
 import StorySegmentReset from '../components/StorySegmentReset';
 
+function extractLayerOverrideIndex(idSchema: {
+  $id?: string;
+}): number | undefined {
+  const id = idSchema?.$id ?? '';
+  const match = id.match(/layerOverride_(\d+)/);
+  return match ? parseInt(match[1], 10) : undefined;
+}
+
+/** Inline field that shows sourceProperties formData for debugging. */
+function SourcePropertiesDebug(props: FieldProps) {
+  const context = props.formContext as
+    | { formData?: IStorySegmentLayer }
+    | undefined;
+  const fullFormData = context?.formData;
+  const index = extractLayerOverrideIndex(props.idSchema ?? {});
+  const sourceProperties =
+    index !== undefined && fullFormData?.layerOverride?.[index]
+      ? fullFormData.layerOverride[index].sourceProperties
+      : undefined;
+  console.log('index', index);
+  console.log('fullFormData', fullFormData);
+  const model = props.formContext.model;
+  const layerId = fullFormData?.layerOverride?.[index ?? 0].targetLayer;
+
+  const layer = model.getLayer(layerId);
+  const sourceID = layer?.parameters?.source;
+  const source = model.getSource(sourceID);
+  console.log('layer', layer);
+  console.log('source', source);
+
+  return React.createElement(
+    'pre',
+    {
+      style: {
+        fontSize: '0.75rem',
+        padding: '0.5rem',
+        background: 'var(--jp-layout-color2)',
+        borderRadius: 4,
+        overflow: 'auto',
+      },
+    },
+    JSON.stringify(sourceProperties ?? null, null, 2),
+  );
+}
+
 export class StorySegmentLayerPropertiesForm extends LayerPropertiesForm {
   protected processSchema(
     data: IStorySegmentLayer | undefined,
@@ -59,6 +104,10 @@ export class StorySegmentLayerPropertiesForm extends LayerPropertiesForm {
         },
         opacity: {
           'ui:field': 'opacity',
+        },
+        sourceProperties: {
+          'ui:field': (fieldProps: FieldProps) =>
+            React.createElement(SourcePropertiesDebug, fieldProps),
         },
       },
       'ui:options': {
